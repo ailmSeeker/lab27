3bde5d61c8181475c1e8141c3e5bdc5c9ca8eda3
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-10 18:41:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names){
+	let returnName = [];
+	for(let x of names){
+		returnName.push(`Thank you, ${x}, for the wonderful surprise gift!`);	
+	}
+
+	return returnName;
+}
+
+function countDown(numbers){
+	for(let x = numbers; x >= 0; x--){
+		console.log(x);
+	}
+}
